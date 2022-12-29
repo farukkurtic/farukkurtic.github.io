635cbaaf4da0181b9a89ad4fec191172dd8c3bc0
faruk21 <farukkurtic@gmail.com>
2022-12-29 21:20:05 +0100

    add new files

---
+++ index.js
@@ -0,0 +1,10 @@
+/* header page */
+
+let typed = new Typed(".typing", {
+    strings: ["frontend developer", "UI/UX designer", "tech enthusiast"],
+    typeSpeed: 185,
+    backSpeed: 185,
+    loop: true
+})
+
+
